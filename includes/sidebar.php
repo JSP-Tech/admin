f0bbac04107f9e85edac7f6c2b02ead34ff02827
Laxman Singh <jsptechnologies@gmail.com>
2018-02-18 13:16:49 +0545

    pages added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,9 +2,9 @@
 <a href="#" class="visible-phone"><i class="icon icon-home"></i> Menus</a>
   <ul>
     <li ><a href="index"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-    <li> <a href="charts.html"><i class="icon icon-bell"></i> <span> Notices</span></a> </li>
-    <li> <a href="widgets.html"><i class="icon icon-user"></i> <span> Management Team</span></a> </li>
-    <li><a href="tables.html"><i class="icon icon-user"></i> <span> Teachers</span></a></li>
-    <li><a href="grid.html"><i class="icon icon-picture"></i> <span> Photo Gallery</span></a></li>
+    <li> <a href="notices"><i class="icon icon-bell"></i> <span> Notices</span></a> </li>
+    <li> <a href="management_team"><i class="icon icon-user"></i> <span> Management Team</span></a> </li>
+    <li><a href="teachers"><i class="icon icon-user"></i> <span> Teachers</span></a></li>
+    <li><a href="gallery"><i class="icon icon-picture"></i> <span> Photo Gallery</span></a></li>
     </ul>
 </div>
